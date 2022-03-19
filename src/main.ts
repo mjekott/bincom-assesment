e6504e5c5afab3d25c1822cc581c14e6f9c09783
mjekott <ekottmfon@yahoo.com>
2022-03-19 14:37:33 +0100

    added changes

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,21 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as session from 'express-session';
 import { flash } from 'express-flash-message';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      },
+    }),
+  );
+  app.use(flash({ sessionKeyName: 'flashMessage' }));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.use(

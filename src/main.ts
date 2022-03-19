7d9d372dcba2d46878d1294f501d543753e369ab
mjekott <ekottmfon@yahoo.com>
2022-03-19 12:21:53 +0100

    updated readme

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
     }),
   );
   app.use(flash({ sessionKeyName: 'flashMessage' }));
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
